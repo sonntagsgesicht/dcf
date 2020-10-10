17f6019c6f979cf2072967c805afbf64527c2be7
sonntagsgesicht <sonntagsgesicht@icloud.com>
2020-10-10 23:16:29 +0200

    release 0.4

--- dcf/__init__.py
+++ dcf/__init__.py
@@ -5,13 +5,13 @@
 # A Python library for generating discounted cashflows.
 # 
 # Author:   sonntagsgesicht, based on a fork of Deutsche Postbank [pbrisk]
-# Version:  0.3, copyright Saturday, 10 October 2020
+# Version:  0.4, copyright Saturday, 10 October 2020
 # Website:  https://github.com/sonntagsgesicht/dcf
 # License:  Apache License 2.0 (see LICENSE file)
 
 
 __doc__ = 'A Python library for generating discounted cashflows.'
-__version__ = '0.3'
+__version__ = '0.4'
 __dev_status__ = '3 - Alpha'
 __date__ = 'Saturday, 10 October 2020'
 __author__ = 'sonntagsgesicht, based on a fork of Deutsche Postbank [pbrisk]'
