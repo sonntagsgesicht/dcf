350479135eac4a857b505fcb612b092a1fef4937
sonntagsgesicht <sonntagsgesicht@icloud.com>
2022-05-21 00:28:09 +0200

    * added/updated documentation

--- dcf/daycount.py
+++ dcf/daycount.py
@@ -14,6 +14,37 @@ DAYS_IN_YEAR = 365.25
 
 
 def day_count(start, end):
+    r""" default day count function for rate period calculation
+
+    :param start: period start date $t_s$
+    :param end: period end date $t_e$
+    :returns: year fraction $\tau(t_s, t_e)$
+        from **start** to **end** as a float
+
+    this default **day_count** function calculates the number of days
+    between $t_s$ and $t_e$ expressed as a fraction of a year, i.e.
+    $$\tau(t_s, t_e) = \frac{t_e-t_s}{365.25}$$
+    as an average year has nearly $365.25$ days.
+
+    Since different date packages have differnet concepts to derive
+    the number of days between two dates, **day_count** tries to adopt
+    at least some of them. As there are:
+
+    * dates given already as year fractions as a
+      `float <https://docs.python.org/3/library/functions.html?#float>`_
+      so $\tau(t_s, t_e) = t_e - t_s$.
+
+    * `datetime <https://docs.python.org/3/library/datetime.html>`_
+      the native Python package, so $\delta = t_e - t_s$ is a **timedelta**
+      object with attribute **days** which is used.
+
+    * `businessdate <https://pypi.org/project/businessdate/>`_
+      a specialised package for banking business calendar
+      and time period calculations,
+      so the **BusinessDate** object **start** has a method
+      **start.diff_in_days** which is used.
+
+    """
     if hasattr(start, 'diff_in_days'):
         # duck typing businessdate.BusinessDate.diff_in_days
         return float(start.diff_in_days(end)) / DAYS_IN_YEAR
--- dcf/plans.py
+++ dcf/plans.py
@@ -15,29 +15,87 @@ FIXED_RATE = 0.01
 
 
 def same(num, amount=DEFAULT_AMOUNT):
+    r""" all same payment plan
+
+    :param num: number of payments $n$
+    :param amount: amount of each payment $N$
+    :return: list(float) payment plan $X_i$ for $i=1 \dots n$
+
+    Payment plan with
+    $$X_i = N \text{ for all } i=1 \dots n$$
+    """
     return [amount] * int(num)
 
 
 def bullet(num, amount=DEFAULT_AMOUNT):
+    r""" bullet payment plan
+
+    :param num: number of payments $n$
+    :param amount: amount of last bullet payment $N$
+    :return: list(float) payment plan $X_i$ for $i=1 \dots n$
+
+    Payment plan with
+    $$X_i = N \text{ for } i=n \text{ else } 0$$
+    """
     return [0.] * (int(num) - 1) + [amount]
 
 
 def amortize(num, amount=DEFAULT_AMOUNT):
+    r""" linear amortize payment plan
+
+    :param num: number of payments $n$
+    :param amount: amount of total sum of payment $N$
+    :return: list(float) payment plan $X_i$ for $i=1 \dots n$
+
+    Payment plan with
+    $$X_i = N/n \text{ for } i=1 \dots n$$
+    """
     return [amount / num] * int(num)
 
 
 def annuity(num, amount=DEFAULT_AMOUNT, fixed_rate=FIXED_RATE):
+    r""" fixed rate annuity payment plan
+
+    :param num: number of payments $n$
+    :param amount: amount of total sum of payment $N$
+    :param fixed_rate: amortization rate $r$
+    :return: list(float) payment plan $X_i$ for $i=1 \dots n$
+
+    Payment plan
+    $$X_i = \frac{r}{(1 + r)^{n-i}} \cdot N \text{ for } i=1 \dots n$$
+    """
     q = 1. + fixed_rate
     a = amount * (q - 1) / (q ** int(num) - 1)
     return list(a * q ** i for i in range(int(num)))
 
 
 def consumer(num, amount=DEFAULT_AMOUNT, fixed_rate=FIXED_RATE):
+    r""" consumer loan annuity payment plan
+
+    :param num: number of payments $n$
+    :param amount: amount of payment total $N$
+    :param fixed_rate: amortization rate $r$
+    :return: list(float) payment plan $X_i$ for $i=1 \dots n$
+
+    Actutal payment plan total $T = N (1 + n \cdot r)$ such that
+    $$X_i = T / n$$
+    """
     total = amount * (1 + num * fixed_rate)
     return [total / num] * int(num)
 
 
 def outstanding(plan, amount=DEFAULT_AMOUNT, sign=False):
+    r"""sums up plans to remaining oustanding anmount
+
+    :param plan: payment plan $X_i$
+    :param amount: inital amount $N$
+    :param sign: $\sigma$ sign of plan payments (optional, default: **-1**)
+    :return: list(float) outstanding plan
+
+    Adds or substracts payment plan payments $X_i$ from inital amount $N$
+    such that
+    $$O_i = N + \sigma \cdot \sum_{k=1}^{i-1} X_i$$
+    """
     sgn = 1 if sign else -1
     out = [amount]
     for p in plan[:-1]:
