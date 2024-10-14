741905cddef41d3ef8a2cc366c54971419708421
Sonntagsgesicht <sonntagsgesicht@icloud.com>
2024-10-14 20:39:09 +0200

    removed products.py

--- dcf/__init__.py
+++ dcf/__init__.py
@@ -13,7 +13,7 @@
 __doc__ = 'A Python library for generating discounted cashflows.'
 __version__ = '0.8'
 __dev_status__ = '4 - Beta'
-__date__ = 'Thursday, 02 June 2022'
+__date__ = 'Monday, 14 October 2024'
 __author__ = 'sonntagsgesicht, based on a fork of Deutsche Postbank [pbrisk]'
 __email__ = 'sonntagsgesicht@icloud.com'
 __url__ = 'https://github.com/sonntagsgesicht/' + __name__
